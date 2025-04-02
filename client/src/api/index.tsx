9bad4db5e67631e744f929979226830e0d2b47ee
Omkar Sonawane <130058150+Omkar-Sonawane-23@users.noreply.github.com>
2025-04-02 18:37:11 +0530

    Update index.tsx

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios"
 
-const pistonBaseUrl = "http://localhost:3000"
+const pistonBaseUrl = "https://emkc.org/api/v2/piston"
 
 const instance: AxiosInstance = axios.create({
     baseURL: pistonBaseUrl,
